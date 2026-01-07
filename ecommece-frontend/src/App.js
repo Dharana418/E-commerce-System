2a10788fd38c6df6e361465eb25f5542e1634679
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-07 18:23:52 +0530

    halfway eneded login frontend

--- ecommece-frontend/src/App.js
+++ ecommece-frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div className="slidebar">
-      </div>
-    </>
+    <div className="App">
+      <div className="slidebar"></div>
+      <h1>ElectroHub</h1>
+      <form className="login-form">
+      </form>
+    </div>
   );
 }
+
 
 export default App;
