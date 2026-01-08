f78d2b08ee3fdccc059c1df40f2d180736d9bd1e
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-08 18:47:20 +0530

    ended frontend login form and implemented raint js framework to it

--- ecommece-frontend/src/App.js
+++ ecommece-frontend/src/App.js
@@ -4,13 +4,61 @@ function App() {
   return (
     <div className="App">
       <div className="slidebar"></div>
-      <h1>ElectroHub</h1>
       <form className="login-form">
-        <label for="username">Username</label><br></br>
-        <input type="text" id="username" name="username" placeholder="Enter your username" /><br></br>
-        <label for="password">Password</label><br></br>
-        <input type="password" id="password" name="password" placeholder="Enter your password" /><br></br>
-        <button type="submit">Login</button>
+        <div className="form-header">
+          <h1>Welcome</h1>
+          <p>E-Commerce Store</p>
+        </div>
+        
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input 
+            type="text" 
+            id="username" 
+            name="username" 
+            placeholder="Enter your username"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+
+        <div className="form-options">
+          <label className="remember-me">
+            <input type="checkbox" name="remember" />
+            Remember me
+          </label>
+          <button 
+            type="button" 
+            className="forgot-link"
+            onClick={(e) => e.preventDefault()}
+          >
+            Forgot password?
+          </button>
+        </div>
+
+        <button type="submit" className="login-btn">Login</button>
+
+        <div className="form-footer">
+          <p>Don't have an account? 
+            <button 
+              type="button" 
+              className="signup-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              Sign up
+            </button>
+          </p>
+        </div>
       </form>
     </div>
   );
