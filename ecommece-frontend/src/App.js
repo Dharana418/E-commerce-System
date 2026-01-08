87b251639dedc1d228b88c40e21055646bddd823
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-08 14:41:04 +0530

    halfway ended frontend

--- ecommece-frontend/src/App.js
+++ ecommece-frontend/src/App.js
@@ -6,6 +6,11 @@ function App() {
       <div className="slidebar"></div>
       <h1>ElectroHub</h1>
       <form className="login-form">
+        <label for="username">Username</label><br></br>
+        <input type="text" id="username" name="username" placeholder="Enter your username" /><br></br>
+        <label for="password">Password</label><br></br>
+        <input type="password" id="password" name="password" placeholder="Enter your password" /><br></br>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
