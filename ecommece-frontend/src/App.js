aeeb0f666efedaaae57bd09d7a03f254b25e11f4
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-10 16:13:50 +0530

    frontend

--- ecommece-frontend/src/App.js
+++ ecommece-frontend/src/App.js
@@ -4,9 +4,6 @@ import Login from './LoginandRegistration/Login';
 function App() {
   return (
     <div className="App">
-      <div className="slidebar">
-        <img src="https://i.ibb.co/4f1y2nQ/eshoply-logo.png" alt="Eshoply Logo" className="logo"/>
-      </div>
       <Login />
     </div>
   );
