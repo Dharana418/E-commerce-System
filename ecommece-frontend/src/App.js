ac38f05686f24058cba4e6e95fd6d232f3c78351
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-10 14:42:40 +0530

    directions changed

--- ecommece-frontend/src/App.js
+++ ecommece-frontend/src/App.js
@@ -1,42 +1,13 @@
 import './App.css';
+import Login from './LoginandRegistration/Login';
 
 function App() {
   return (
     <div className="App">
-      <div className="slidebar"></div>
-      <form className="login-form">
-        <div className="form-header">
-          <h1>Welcome to the EShoply</h1>
-        </div>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input 
-            type="text" 
-            id="username" 
-            name="username" 
-            placeholder="Enter your username"
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            id="password" 
-            name="password" 
-            placeholder="Enter your password"
-            required
-          />
-        </div>
-
-        <button type="submit" className="login-btn">Login</button>
-
-        <div className="form-footer">
-          <p>Don't have an account? 
-          </p>
-        </div>
-      </form>
+      <div className="slidebar">
+        <img src="https://i.ibb.co/4f1y2nQ/eshoply-logo.png" alt="Eshoply Logo" className="logo"/>
+      </div>
+      <Login />
     </div>
   );
 }
---
+++ ecommece-frontend/src/LoginandRegistration/Login.jsx
@@ -0,0 +1,41 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <form className="login-form">
+      <div className="form-header">
+        <h1>Welcome to the EShoply</h1>
+      </div>
+      <div className="form-group">
+        <label htmlFor="username">Email</label>
+        <input 
+          type="text" 
+          id="username" 
+          name="username" 
+          placeholder="Enter your email"
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input 
+          type="password" 
+          id="password" 
+          name="password" 
+          placeholder="Enter your password"
+          required
+        />
+      </div>
+
+      <button type="submit" className="login-btn">Login</button>
+
+      <div className="form-footer">
+        <p>Don't have an account? 
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default Login;
