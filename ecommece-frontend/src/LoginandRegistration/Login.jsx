7d17eb0cd3860a1bceb7b40860ded5502da68760
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-12 20:15:12 +0530

    #4

--- ecommece-frontend/src/LoginandRegistration/Login.jsx
+++ ecommece-frontend/src/LoginandRegistration/Login.jsx
@@ -6,10 +6,6 @@ function Login() {
   return (
     <>
     <div className="background">
-      <div class="custom-shape-divider-bottom-1768175439">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-    </svg>
     </div>
     <div className="slidebar">
       <img 
@@ -50,7 +46,7 @@ function Login() {
           <p>Don't have an account?  <h5><u><span className="register-link">Register here</span></u></h5></p>
         </div>
       </form>
-      </div>
+    
     </>
   );
 }
