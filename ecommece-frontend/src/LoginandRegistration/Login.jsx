9fbf7ec1fa755240d4ec09587a8ffe1b58cc47e4
Dharana Thilakarathna <Thiyunuwan567@gmail.com>
2026-01-12 22:24:44 +0530

    finished login frontend

--- ecommece-frontend/src/LoginandRegistration/Login.jsx
+++ ecommece-frontend/src/LoginandRegistration/Login.jsx
@@ -1,11 +1,16 @@
 import './Login.css';
 
 function Login() {
-  const cloudinaryImageUrl = 'https://res.cloudinary.com/dttczxa2i/image/upload/2148688560_2_pmhwdx';
+  const cloudinaryImageUrl = 'https://res.cloudinary.com/dttczxa2i/image/upload/ChatGPT_Image_Jan_12_2026_09_32_37_PM_tyycuu';
 
   return (
     <>
     <div className="background">
+      <div class="custom-shape-divider-bottom-1768234483">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
     </div>
     <div className="slidebar">
       <img 
